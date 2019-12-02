b2e25c0f187e6f85f51b269a9ad1802e488cff90
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-02 16:16:27 +0100

    Migration down for devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   belongs_to :city
   belongs_to :objective
   has_many :data
