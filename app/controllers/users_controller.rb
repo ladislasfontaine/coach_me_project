84072a45ad33f08ca68743496a1aada6ce49fc50
remi <rr03ml@gmail.com>
2019-12-02 18:29:51 +0100

    view_show_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
 
   def welcome
+
   end
 
+
+
   def show
+    @user = User.find(params[:id])
+    @seances = Seance.all
+  end
+
+
+  def edit
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "users#welcome"
+  resources :users
 
 end
