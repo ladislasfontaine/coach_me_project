97529d32bb04d2152f020d479cc5091c127b340b
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-02 17:42:14 +0100

    Devise coach up and running

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,8 +1,13 @@
 class Coach < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
 	belongs_to :city, optional: true
 	has_many :coach_specialies
 	has_many :specialties, through: :coach_specialies
-	has_many :users, through: :seance
+	has_many :seances
+  has_many :users, through: :seances
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_150714) do
+ActiveRecord::Schema.define(version: 2019_12_02_161630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 2019_12_02_150714) do
     t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["city_id"], name: "index_coaches_on_city_id"
   end
 
