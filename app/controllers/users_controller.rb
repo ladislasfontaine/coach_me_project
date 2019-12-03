7029694962950f575ed0bcd19e2e3215cb037670
remi <rr03ml@gmail.com>
2019-12-03 13:10:03 +0100

    user method update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
   end
 
 
-  def updated
-
-
+  def update
+    @user = User.find(params[:id])
+    @user.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], birth_date: params[:birth_date], phone_number: params[:phone_number], address: params[:address], city: params[:city], zip_code: params[:zip_code], description: params[:description], objective: params[:objective])
+    redirect_to @user
   end
 end
