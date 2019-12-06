0e9aa5a362e4e86f1d7e0009a2286606b3b3b599
remi <rr03ml@gmail.com>
2019-12-06 15:33:19 +0100

    routes statics

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :coaches
   devise_for :users
   resources :coaches
-  resources :users 
+  resources :users
   resources :seances
-   
   root to: "users#welcome"
+    get "/contacts", to: 'statics#contacts'
+    get "/cgv", to: 'statics#cgv'
+    get "/about", to: 'statics#about'
 end
