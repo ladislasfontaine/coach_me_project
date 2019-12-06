3485539a9f1017231208a1386f9a34edce91e9c6
remi <rr03ml@gmail.com>
2019-12-06 15:52:06 +0100

    routes statics

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   resources :coaches
   resources :users
   resources :seances
-
   resources :objectives
-
   root to: "users#welcome"
+  
     get "/contacts", to: 'statics#contacts'
     get "/cgv", to: 'statics#cgv'
     get "/about", to: 'statics#about'
