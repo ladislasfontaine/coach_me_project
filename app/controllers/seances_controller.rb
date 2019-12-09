8d9dc1d2f347226a8125659a8996128785e14d67
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-09 18:49:00 +0100

    Overlapping function working

--- app/controllers/seances_controller.rb
+++ app/controllers/seances_controller.rb
@@ -4,7 +4,9 @@ class SeancesController < ApplicationController
   def create
     @coach = Coach.find(params[:coach_id])
     @seance = Seance.new(coach: @coach, user: current_user, start_date: params[:start_date], duration: params[:duration], place: params[:place])
-    if @seance.save
+    if is_overlapping?(params[:start_date], params[:duration])
+      flash[:notice] = "Une séance est déjà réservée sur ce créneau."
+    elsif @seance.save
       flash[:notice] = "La séance est créée. Merci d'avoir réservé avec CoachMe."
     else
       flash[:notice] = "Un problème est survenu."
@@ -32,5 +34,23 @@ class SeancesController < ApplicationController
 
   def set_seance
     @seance = Seance.find(params[:id])
+  end
+
+  def is_overlapping?(start, duration)
+    time_array = start.split(/[-, T, :]/)
+    seance_start = Time.new(time_array[0], time_array[1], time_array[2], time_array[3], time_array[4]).strftime('%s').to_i
+    seance_end = seance_start + (duration.to_i * 60)
+    
+    @coach = Coach.find(params[:coach_id])
+    @seances = @coach.seances
+    @seances.each do |seance|
+      instance_start = seance.start_date.strftime('%s').to_i
+      instance_end = instance_start + (duration.to_i * 60)
+      if (seance_start..seance_end).overlaps?(instance_start..instance_end)
+        return true
+        break
+      end
+    end
+    return false
   end
 end
