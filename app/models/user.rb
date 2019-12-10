082926179ea057fac2deb4a4db3b9cea91e465bf
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-10 13:51:29 +0100

    Save facebook changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
-  belongs_to :city, optional: true
+  belongs_to :city, optional: true 
   belongs_to :objective, optional: true
   has_many :data
   has_many :seances
