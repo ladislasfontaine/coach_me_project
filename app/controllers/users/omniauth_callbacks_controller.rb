085559057fd940fede813395eb40944e2c666908
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-11 12:03:51 +0100

    Login with facebook ok

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-
+    
     @user = User.from_facebook(request.env['omniauth.auth'])
 
     if @user.persisted?
@@ -11,7 +11,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session['devise.facebook'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
-
-    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,11 @@ class User < ApplicationRecord
   end
 
   def self.from_facebook(auth)
-    where
+    where(facebook_id: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[6, 20]
+      user.first_name = auth.info.name.split[0] 
+      user.last_name = auth.info.name.split[1]
+    end
   end
 end
