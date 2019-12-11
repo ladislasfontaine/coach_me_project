680a69a432daac00daa3b0b339a3210e58bf7f43
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-11 15:23:59 +0100

    Save changes for google auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,29 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    
     @user = User.from_facebook(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      flash[:notice] = 'Connexion réussie !'
+      flash[:notice] = 'Connexion avec Facebook réussie !'
     else
       session['devise.facebook'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def google_oauth2
+    puts "=============="
+    puts request.env['omniauth.auth'].inspect
+    puts "=============="
+
+    @user = User.from_google(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      flash[:notice] = 'Connexion avec Google réussie !'
+    else
+      session['devise.google'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
